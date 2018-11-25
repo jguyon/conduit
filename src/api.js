92e6089a7a2c7e8f2aa34fc57c6002cfa7e10b98
Jérôme Guyon <jeromep.guyon@gmail.com>
2018-11-25 10:59:28 +0100

    Fix api call to delete article

--- src/api.js
+++ src/api.js
@@ -326,7 +326,12 @@ export const updateArticle = (
   });
 
 export const deleteArticle = (token: string, slug: string): Promise<void> =>
-  fetch(`${ENDPOINT}/articles/${encodeURIComponent(slug)}`).then(response => {
+  fetch(`${ENDPOINT}/articles/${encodeURIComponent(slug)}`, {
+    method: "DELETE",
+    headers: new Headers({
+      authorization: `Token ${token}`
+    })
+  }).then(response => {
     if (response.status !== 200) {
       throw new Error(`expected status 200 but got ${response.status}`);
     }
